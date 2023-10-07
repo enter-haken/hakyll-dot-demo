249b87692e110d0d95b4913607ef666d6782f5fe
Sergei Trofimovich <slyich@gmail.com>
2023-10-07 12:04:19 +0100

    site.hs: use Compiler monad instead of unsafePerformIO
    And while at it use unixFilter instead of direct readProcess call.

--- site.hs
+++ site.hs
@@ -2,10 +2,8 @@
 import qualified Data.Text as T
 import Hakyll
 import Text.Pandoc
-import System.Process ( readProcess )
-import System.IO.Unsafe ( unsafePerformIO )
 
-import Text.Pandoc.Walk ( walk )
+import Text.Pandoc.Walk ( walkM )
 
 main :: IO ()
 main = hakyll $ do
@@ -17,20 +15,19 @@ main = hakyll $ do
     match "template.html" $ compile templateCompiler
 
 pandocPostCompiler :: Compiler (Item String)
-pandocPostCompiler = pandocCompilerWithTransform
+pandocPostCompiler = pandocCompilerWithTransformM
     defaultHakyllReaderOptions
     defaultHakyllWriterOptions
     graphViz
 
-graphViz :: Pandoc -> Pandoc
-graphViz = walk codeBlock
+graphViz :: Pandoc -> Compiler Pandoc
+graphViz = walkM codeBlock
 
-codeBlock :: Block -> Block
-codeBlock cb@(CodeBlock (id, classes, namevals) contents) = 
-    case lookup "lang" namevals of
-        Just f -> RawBlock (Format "html") $ svg contents
-        nothing -> cb
-codeBlock x = x
+codeBlock :: Block -> Compiler Block
+codeBlock (CodeBlock (_id, _classes, namevals) contents)
+    | ("lang", "dot") `elem` namevals
+    = RawBlock (Format "html") <$> svg contents
+codeBlock x = return x
 
-svg :: T.Text -> T.Text
-svg contents = T.pack $ unsafePerformIO $ readProcess "dot" ["-Tsvg"] (T.unpack contents)
+svg :: T.Text -> Compiler T.Text
+svg contents = T.pack <$> unixFilter "dot" ["-Tsvg"] (T.unpack contents)
